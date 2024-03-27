748764e2f0e1bb28a9f4826a3d5539ee5d4a96db
Zaid Abu Samra <160019673+ZaidAbuSamraa@users.noreply.github.com>
2024-03-27 19:16:26 +0200

    Update Admin.java

--- src/main/java/Entity/Admin.java
+++ src/main/java/Entity/Admin.java
@@ -1,14 +1,14 @@
 package Entity;
 
 public class Admin {
-    final private String A = "Zaid" ;
-    final private String P ="123456";
+    final private String my_a = "Zaid" ;
+    final private String my_pp ="123456";
 
-    public String getA() {
+    public String getmy_a() {
         return A;
     }
 
-    public String getP() {
+    public String getmy_pp() {
         return P;
     }
 }
