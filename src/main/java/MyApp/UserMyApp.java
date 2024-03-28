9b45b4f5fe8d8fd9c6a20ff14d7cde5764940f5d
Zaid Abu Samra <160019673+ZaidAbuSamraa@users.noreply.github.com>
2024-03-29 00:51:34 +0200

    Update UserMyApp.java

--- src/main/java/MyApp/UserMyApp.java
+++ src/main/java/MyApp/UserMyApp.java
@@ -4,8 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class UserMyApp {
     private static final Logger logger = LogManager.getLogger(UserMyApp.class);
@@ -13,30 +11,18 @@ public class UserMyApp {
     private static final String ERROR_CLOSING_READER = "Error closing the reader: {}";
 
     public static void displayFileContents(String filePath) {
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(filePath));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 logger.info(line);
             }
         } catch (IOException e) {
             logger.error(ERROR_READING_FILE, e.getMessage());
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
     }
 
     public static boolean checkFile(String filename, String typeEvent, String eventName) {
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(filename));
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(":");
@@ -50,22 +36,12 @@ public class UserMyApp {
             }
         } catch (IOException e) {
             logger.error(ERROR_READING_FILE, e.getMessage());
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
         return false;
     }
 
     public static boolean checkHallandDate(String fileName, String date, String eventName) {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
@@ -78,41 +54,21 @@ public class UserMyApp {
             }
         } catch (IOException e) {
             logger.error(ERROR_READING_FILE, e.getMessage());
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
         return false;
     }
 
     public static void AddtoEvent(String name, String item) {
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter("Event.txt", true));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Event.txt", true))) {
             writer.write(name + item);
             writer.newLine();
         } catch (IOException e) {
             logger.error("Error writing to the file: {}", e.getMessage());
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
     }
 
     public static boolean checkFile(String word, String filename) {
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(filename));
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
@@ -122,42 +78,22 @@ public class UserMyApp {
             }
         } catch (IOException e) {
             logger.error(ERROR_READING_FILE, e.getMessage());
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
         return false;
     }
 
     public static void addPackageToFile(String packageName, String filename) {
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(filename, true));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
             writer.write(packageName);
             writer.newLine();
             logger.info("Package '{}' added to file '{}'", packageName, filename);
         } catch (IOException e) {
             logger.error("Error occurred while writing to file: {}", e.getMessage());
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
     }
 
     public static int getColumnValueForHall(String filePath, String hallName, int columnIndex) throws IOException {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(filePath));
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] columns = line.split(",");
@@ -172,22 +108,12 @@ public class UserMyApp {
                 }
             }
             throw new IllegalArgumentException("Hall " + hallName + " not found in file");
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
     }
 
     public static int checkPrise(String filename, int value, int columnIndex) throws IOException {
-        BufferedReader reader = null;
         int counter = 0;
-        try {
-            reader = new BufferedReader(new FileReader(filename));
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
@@ -199,27 +125,16 @@ public class UserMyApp {
                     }
                 }
             }
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
         return counter;
     }
 
     public static boolean deleteLineFromFile(String name, String filename) {
-        BufferedReader reader = null;
-        BufferedWriter writer = null;
         File inputFile = new File(filename);
         File tempFile = new File("temp.txt");
         boolean deleted = false;
-        try {
-            reader = new BufferedReader(new FileReader(inputFile));
-            writer = new BufferedWriter(new FileWriter(tempFile));
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
                 if (currentLine.contains(name)) {
@@ -231,17 +146,6 @@ public class UserMyApp {
         } catch (IOException e) {
             logger.error("Error occurred while deleting line from file: {}", e.getMessage());
             return false;
-        } finally {
-            try {
-                if (writer != null) {
-                    writer.close();
-                }
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                logger.error(ERROR_CLOSING_READER, e.getMessage());
-            }
         }
         if (deleted) {
             if (!inputFile.delete()) {
@@ -258,9 +162,7 @@ public class UserMyApp {
     }
 
     public static void searchValueInFile(String filePath, String searchValue) {
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(filePath));
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
@@ -271,14 +173,6 @@ public class UserMyApp {
             }
         } catch (IOException e) {
             logger.error(ERROR_READING_FILE, e.getMessage());
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    logger.error(ERROR_CLOSING_READER, e.getMessage());
-                }
-            }
         }
     }
 }
