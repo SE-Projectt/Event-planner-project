e6bb5483c3ee826dfb6ef785a1e6d1f213ce0a85
Zaid Abu Samra <160019673+ZaidAbuSamraa@users.noreply.github.com>
2024-03-28 22:24:36 +0200

    Update AddProviderMyApp.java

--- src/main/java/MyApp/AddProviderMyApp.java
+++ src/main/java/MyApp/AddProviderMyApp.java
@@ -3,17 +3,24 @@ package MyApp;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class AddProviderMyApp {
+
+    private static final Logger LOGGER = Logger.getLogger(AddProviderMyApp.class.getName());
 
     public boolean iiEnterValidUsernamePassword(String username, String password){
 
         try (PrintWriter writer = new PrintWriter(new FileWriter("provider_data.txt", true))) {
             // Append the new user's data to the file
             writer.println(username + "," + password);
-            System.out.println("You have signed up successfully!");
+            LOGGER.info("You have signed up successfully!");
         } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
-        }          return  true;}
-
+            LOGGER.severe("Error writing to file: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
 }
