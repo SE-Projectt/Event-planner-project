89e56db19bb166da0f9d34b99a6a78d0f91f7310
Zaid Abu Samra <160019673+ZaidAbuSamraa@users.noreply.github.com>
2024-03-30 00:25:46 +0300

    Update PlanningWeddingTest.java

--- src/test/java/org/example/AcceptanceTest/PlanningWeddingTest.java
+++ src/test/java/org/example/AcceptanceTest/PlanningWeddingTest.java
@@ -1,6 +1,6 @@
 package org.example.AcceptanceTest;
 
-import MyApp.UserMyApp;
+import myapp.UserMyApp;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
