f602cbcf6a42f866f27c81b6b6a3fe918579d4ff
Zaid Abu Samra <160019673+ZaidAbuSamraa@users.noreply.github.com>
2024-03-29 17:01:19 +0300

    Create Admin.java

---
+++ src/main/java/MyApp/Admin.java
@@ -0,0 +1,14 @@
+package MyApp;
+
+public class Admin {
+    final private String myA = "Zaid" ;
+    final private String myPP ="123456";
+
+    public String getmyA() {
+        return myA;
+    }
+
+    public String getmyPP() {
+        return myPP;
+    }
+}
