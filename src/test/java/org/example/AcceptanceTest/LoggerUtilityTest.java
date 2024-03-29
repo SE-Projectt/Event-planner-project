0036c72b49cf03e9b8b08bef1bd3b7ddc5a08a52
Zaid Abu Samra <160019673+ZaidAbuSamraa@users.noreply.github.com>
2024-03-29 17:37:00 +0300

    Update LoggerUtilityTest.java

--- src/test/java/org/example/AcceptanceTest/LoggerUtilityTest.java
+++ src/test/java/org/example/AcceptanceTest/LoggerUtilityTest.java
@@ -1,36 +1,41 @@
+import io.cucumber.java.en.*;
 import static org.junit.Assert.*;
-import org.junit.*;
 import java.util.logging.*;
-import java.io.ByteArrayOutputStream;
-import java.io.UnsupportedEncodingException;
-import org.example.LoggerUtility;
 
-public class LoggerUtilityTest {
+public class LoggerUtilityStepDefinitions {
     private Logger logger;
-    private ByteArrayOutputStream logOutput;
+    private String logMessage;
+    private boolean logMessageCaptured;
 
-    @Before
-    public void setUp() {
-        logger = LoggerUtility.getLogger();
-        logOutput = new ByteArrayOutputStream();
-        Handler memoryHandler = null;
-        try {
-            memoryHandler = new MemoryHandler(new StreamHandler(logOutput, new SimpleFormatter()), 0);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+    @Given("the logger is initialized")
+    public void initializeLogger() {
+        logger = Logger.getLogger(LoggerUtility.class.getName());
+    }
+
+    @When("a message {string} is logged at INFO level")
+    public void logMessage(String message) {
+        LoggerUtility.logInfo(logger, message);
+        logMessage = message;
+    }
+
+    @Then("the log message {string} should be captured")
+    public void verifyLogMessage(String expectedMessage) {
+        logMessageCaptured = false;
+        for (Handler handler : logger.getHandlers()) {
+            if (handler instanceof MemoryHandler) {
+                MemoryHandler memoryHandler = (MemoryHandler) handler;
+                String loggedMessage = new String(memoryHandler.getPushedRecords().get(0).getMessage());
+                if (loggedMessage.contains(expectedMessage)) {
+                    logMessageCaptured = true;
+                    break;
+                }
+            }
         }
-        logger.addHandler(memoryHandler);
+        assertTrue("Log message not captured: " + logMessage, logMessageCaptured);
     }
 
-    @Test
-    public void testGetLogger() {
-        assertNotNull(logger);
-        assertEquals(LoggerUtility.getLogger(), logger);
-        assertEquals(logger.getName(), LoggerUtility.class.getName());
-    }
-
-    @Test
-    public void testSetupLogger() {
+    @When("the logger setup is verified")
+    public void verifyLoggerSetup() {
         assertFalse(logger.getUseParentHandlers());
         Handler[] handlers = logger.getHandlers();
         boolean consoleHandlerFound = false;
@@ -41,18 +46,5 @@ public class LoggerUtilityTest {
             }
         }
         assertTrue(consoleHandlerFound);
-    }
-
-    @Test
-    public void testLogInfo() {
-        // Assuming logger level is set to INFO
-        logger.setLevel(Level.INFO);
-        LoggerUtility.logInfo(logger, "Test message: %s", "arg1");
-
-        // Flush the memory handler to ensure the log message is written to the ByteArrayOutputStream
-        logger.getHandlers()[0].flush();
-
-        // Verify log message
-        assertTrue(logOutput.toString().contains("Test message: arg1"));
     }
 }
