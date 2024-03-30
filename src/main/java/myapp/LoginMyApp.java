a5875975545e7c27a75cf6a29a0edd587ad66709
Zaid Abu Samra <160019673+ZaidAbuSamraa@users.noreply.github.com>
2024-03-30 22:13:18 +0300

    Update LoginMyApp.java

--- src/main/java/myapp/LoginMyApp.java
+++ src/main/java/myapp/LoginMyApp.java
@@ -1,55 +1,14 @@
-package myapp;
+package MyApp;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.logging.Logger;
-import java.util.logging.Level;
+
 
 public class LoginMyApp {
     private static final String MY_A = "Zaid";
     private static final String MY_P = "123456";
     private static final Logger logger = Logger.getLogger(LoginMyApp.class.getName());
 
-    public static boolean iEnterValidUsernameAndIncorrectPassword(String username, String password) {
-        String filePath = "user_data.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length >= 2 && parts[0].equals(username) && !(parts[1].equals(password))) {
-                    return true; // Username and password combination found in the file
-                }
-            }
-        } catch (IOException e) {
-            // Log the error
-            logger.log(Level.SEVERE, "An IOException occurred while reading user data", e);
-        } catch (Exception ex) {
-            // Log any other exceptions
-            logger.log(Level.SEVERE, "An unexpected exception occurred", ex);
-        }
-        return false;
-    }
 
-    public boolean theSystemHasRegisteredUserWithUsernameAndPassword(String username, String password) {
-        String filePath = "user_data.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length >= 2 && parts[0].equals(username) && (parts[1].equals(password))) {
-                    return true;
-                }
-            }
-        } catch (IOException e) {
-            // Log the error
-            logger.log(Level.SEVERE, "An IOException occurred while reading user data", e);
-        } catch (Exception ex) {
-            // Log any other exceptions
-            logger.log(Level.SEVERE, "An unexpected exception occurred", ex);
-        }
-        return false;
-    }
 
     public static boolean iEnterUsernameAndPassword(String adminname, String pata) {
         return adminname.equals(MY_A) && pata.equals(MY_P);
